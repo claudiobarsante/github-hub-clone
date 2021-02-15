cc02bb7ed39be49cd9f567e3ee27f93c5d78a528
Claudio Barsante <clbmribas@gmail.com>
2021-02-15 20:33:51 -0300

    starting implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./pages/Profile/index";
 
 function App() {
-  return <h1>Hello!</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Profile} />
+        <Route path='/:username' component={Profile} />
+        <Route path='/:username/:reponame' component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
